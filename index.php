f59d9106af4b4224a1c99023322a2637c4f17f18
Wai Lam <leftcross112@gmail.com>
2026-07-08 18:45:56 +0800

    Implement user registration and login functionality with error handling

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php
 session_start();
 
+$errors = [
+    "login"=> $_SESSION['login_error'] ?? '',
+    "register"=> $_SESSION['register_error'] ?? ''
+];
+$activeForm = $_SESSION['activeForm'] ?? 'login';
+
+session_unset();
+function displayError(string $formerror) {
+    if (!empty($formerror)) {
+        return !empty($formerror) ? "<p class='p-2 bg-red-600 text-white'>$formerror</p>" :"";
+    }
+}
+
+function isActiveForm(string $formName, string $activeForm) {
+    return $formName === $activeForm ? '' : 'hidden';
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -13,11 +30,12 @@ session_start();
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body>
-    <section id="login-section" class="loginform">
+    <section id="login-section" class="loginform <?= isActiveForm('login', $activeForm); ?>">
         <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
             <div class="flex flex-col justify-center items-center bg-yellow-100/10 p-10 rounded-2xl shadow-lg gap-5">
             <h1 class="text-4xl uppercase font-bold select-none">Login</h1>
             <form action="login_register.php" method="post" class="flex flex-col p-10 gap-2">
+                <?= displayError($errors['login']) ?>
                 <input type="email" name="email" placeholder="Email" required class="border rounded p-0.5">
                 <input type="password" name="password" placeholder="Password" required class="border rounded p-0.5">
                 <button type="submit" name="login" class="border border-black rounded bg-cyan-400 text-white p-1.5 font-semibold hover:scale-105">Login</button>
@@ -28,11 +46,13 @@ session_start();
     </section>
 
 
-    <section id="register-section" class="loginform hidden">
+    <section id="register-section" class="loginform <?= isActiveForm('register', $activeForm); ?>">
         <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
             <div class="flex flex-col justify-center items-center bg-yellow-100/10 p-10 rounded-2xl shadow-lg gap-5">
                 <h1 class="text-4xl uppercase font-bold select-none">Register</h1>
+                <?= displayError($errors['register']) ?>
                 <form action="login_register.php" method="post" class="flex flex-col p-10 gap-2">
+                    <input type="text" name="username" placeholder="Username" required class="border rounded p-0.5">
                     <input type="email" name="email" placeholder="Email" required class="border rounded p-0.5">
                     <input type="password" name="password" placeholder="Password" required class="border rounded p-0.5">
                     <button type="submit" name="register" class="border border-black rounded bg-cyan-400 text-white p-1.5 font-semibold hover:scale-105">Register</button>
--- login_register.php
+++ login_register.php
@@ -2,8 +2,56 @@
 session_start();
 require_once("config.php");
 
+if(isset($_POST["register"])) {
+    $name = $_POST["username"];
+    $email = $_POST["email"];
+    $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+    $role = $_POST["role"] ?? 'member';
 
+    $stmt = $conn->prepare("SELECT * FROM users WHERE email = ?");
+    $stmt -> bind_param("s", $email);
+    $stmt -> execute();
+    
+    $checkEmail = $stmt-> get_result();
+    if($checkEmail->num_rows > 0) {
+        $_SESSION["register_error"] = "Email already exists!";
+        $_SESSION["activeForm"] = "register";
+    } else {
+        $stmt = $conn->prepare("INSERT INTO users (name, email, password, role) VALUES (?, ?, ?, ?)");
+        $stmt -> bind_param("ssss", $name, $email, $password, $role);
+        $stmt -> execute();
 
+    }
+    header("Location: index.php");
+    exit();
+}
 
+if(isset($_POST["login"])) {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $stmt = $conn->prepare("SELECT * FROM users WHERE email = ?");
+    $stmt -> bind_param("s", $email);
+    $stmt -> execute();
+    
+    $result = $stmt-> get_result();
+    if($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+        if(password_verify($password, $user["password"])) {
+            $_SESSION["user_name"] = $user["name"];
+            $_SESSION["user_role"] = $user["role"];
+            header("Location: user_page.php");
+            exit();
+        } else {
+            $_SESSION["login_error"] = "Incorrect password!";
+            $_SESSION["activeForm"] = "login";
+            header("Location: index.php");
+        }
+    } else {
+        $_SESSION["login_error"] = "Email not found!";
+        $_SESSION["activeForm"] = "login";
+        header("Location: index.php");
+    }
+}
 
 ?>
